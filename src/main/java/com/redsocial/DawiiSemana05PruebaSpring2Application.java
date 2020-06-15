06771474c2bb7b5c74fd1c3d3ab6572dc08b5a42
christianwcabrerav <catp.ccabrerav@gmail.com>
2020-06-15 17:50:53 -0500

    first commit

---
+++ src/main/java/com/redsocial/DawiiSemana05PruebaSpring2Application.java
@@ -0,0 +1,13 @@
+package com.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiSemana05PruebaSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiSemana05PruebaSpring2Application.class, args);
+	}
+
+}
